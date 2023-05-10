1817041b1b4b7414d474bc2620c8053ec502c5de
sowmi <tamilsowmi1010@gmail.com>
2023-05-10 20:25:24 +0600

    live search

--- lib/search_form_web/bus_live.ex
+++ lib/search_form_web/bus_live.ex
@@ -1,12 +1,22 @@
 defmodule SearchFormWeb.BusLive do
   use SearchFormWeb, :live_view
 
+  def mount(_params, _session, socket) do
+    socket =
+      assign(socket,
+        search_bus: "",
+        buses: []
+      )
+
+    {:ok, socket}
+  end
+
   def render(assigns) do
     ~L"""
     <div>
       <h2 class="font-sans font-bold text-center text-2xl text-purple-600">In the search box, enter the point of origin and destination</h2>
-      <form phx-submit="search" phx-change="search" phx-value-from="search_bus"  class="mt-4 flex gap-2">
-        <input type="text" name="search_bus"  autofocus autocomplete="off" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500" placeholder="FROM" />
+      <form phx-submit="search" phx-change="search" phx-value="search_bus"  class="mt-4 flex gap-2">
+        <input type="text" name="search_bus" autofocus autocomplete="off" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500" placeholder="FROM" />
         <input type="text" name="search_bus" autofocus autocomplete="off" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500" placeholder="TO" />
         <button type="submit" class="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto">Submit</button>
      </form>
@@ -36,21 +46,24 @@ defmodule SearchFormWeb.BusLive do
     """
   end
 
-  def mount(_params, _session, socket) do
-    socket =
-      assign(socket,
-        search_bus: "",
-        buses: []
-      )
-
-    {:ok, socket}
+  def filtered_buses(search_bus) do
+    search_bus
+    |> String.downcase()
+    |> filter_by_from()
+    |> filter_by_to()
   end
 
-  def filtered_buses(search_bus) do
+  defp filter_by_to(search_bus) do
     getBuses()
     |> Enum.filter(fn bus ->
-      String.downcase(bus.from) == String.downcase(search_bus) ||
-        String.downcase(bus.to) == String.downcase(search_bus)
+      String.downcase(bus.to) == search_bus
+    end)
+  end
+
+  defp filter_by_from(search_bus) do
+    getBuses()
+    |> Enum.filter(fn bus ->
+      String.downcase(bus.from) == search_bus
     end)
   end
 
@@ -59,18 +72,18 @@ defmodule SearchFormWeb.BusLive do
     {:noreply, assign(socket, buses: filtered_buses)}
   end
 
-  def getBuses do
+  defp getBuses do
     [
       %{from: "Theni", to: "Coimbatore", price: "1099", timing: "9:00 AM"},
       %{from: "Coimbatore", to: "Chennai", price: "1099", timing: "10:30 AM"},
       %{from: "Villupuram", to: "Chennai", price: "980", timing: "12:00 PM"},
-      %{from: "Chennai", to: "Villupuram", price: "980", timing: "02:00 PM"},
-      %{from: "Kanyakumari", to: "Theni", price: "1139", timing: "07:00 AM"},
-      %{from: "Villupuram", to: "Thiruvarur", price: "1139", timing: "09:00 PM"},
+      %{from: "Chennai", to: "Villupuram", price: "980", timing: "2:00 PM"},
+      %{from: "Kanyakumari", to: "Theni", price: "1139", timing: "7:00 AM"},
+      %{from: "Villupuram", to: "Thiruvarur", price: "1139", timing: "9:00 PM"},
       %{from: "Villupuram", to: "Thiruvarur", price: "1139", timing: "12:30 PM"},
-      %{from: "Kanyakumari", to: "Theni", price: "1139", timing: "09:00 PM"},
-      %{from: "Chennai", to: "Coimbatore", price: "1099", timing: "08:00 AM"},
-      %{from: "Theni", to: "Villupuram", price: "1139", timing: "07:00 AM"}
+      %{from: "Kanyakumari", to: "Theni", price: "1139", timing: "9:00 PM"},
+      %{from: "Chennai", to: "Coimbatore", price: "1099", timing: "8:00 AM"},
+      %{from: "Theni", to: "Villupuram", price: "1139", timing: "7:00 AM"}
     ]
   end
 end
